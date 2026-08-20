64972f4e661f56d0cadb5c5ef32f846e9f47a3e0
/Maycon Vyctor <mayconsantosfc11@gmail.com>
2026-08-20 02:57:12 -0300

    feat(frontend): add routing, navigation bar and invoice component skeleton

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import {Stock} from "./components/stock/stock";
+import {Invoice} from "./components/invoice/invoice";
 
 export const routes: Routes = [
     {
@@ -11,5 +12,11 @@ export const routes: Routes = [
         path: '',
         redirectTo: '/stock',
         pathMatch: 'full'
-    }
+    },
+
+    { 
+        path: 'invoice', 
+        component: Invoice 
+    },
+    
 ];
